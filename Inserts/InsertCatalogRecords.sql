cbb615e2999d40fde6d490848ceb0dab2ae3f37f
jenapung <ahh.its.nena@hotmail.com>
2014-12-07 17:48:04 -0600

    Create InsertCatalogRecords.sql

---
+++ Inserts/InsertCatalogRecords.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO Catalog
+(Cat_ID, Title, Category, Rent_Code)
+VALUES('C001', 'The Matrix', 'Action', 'RC001');
+
+INSERT INTO Catalog
+(Cat_ID, Title, Category, Rent_Code)
+VALUES('C002', 'Frozen', 'Family', 'RC003');
+
+INSERT INTO Catalog
+(Cat_ID, Title, Category, Rent_Code)
+VALUES('C003', 'Mirror', 'Horror', 'RC003');
+
+INSERT INTO Catalog
+(Cat_ID, Title, Category, Rent_Code)
+VALUES('C004', 'Liar Liar', 'Comedy', 'RC002');
