9405cff432b99c7d4bb18bdf3eca6c32a32327e0
jenapung <ahh.its.nena@hotmail.com>
2014-12-07 17:46:21 -0600

    Create InsertRental_codeRecords.sql

---
+++ Inserts/InsertRental_codeRecords.sql
@@ -0,0 +1,12 @@
+INSERT INTO Rental_Code 
+(Rent_Code, Rent_Fee)
+VALUES('RC001', 3.97);
+
+INSERT INTO Rental_Code 
+(Rent_Code, Rent_Fee)
+VALUES('RC002', 1.99);
+
+INSERT INTO Rental_Code 
+(Rent_Code, Rent_Fee)
+VALUES('RC003', 2.59);
+
