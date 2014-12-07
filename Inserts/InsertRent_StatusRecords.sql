4e7cc8d8d8f3f72a86207e985ce2413e58c40c1d
jenapung <ahh.its.nena@hotmail.com>
2014-12-07 17:51:40 -0600

    Create InsertRent_StatusRecords.sql

---
+++ Inserts/InsertRent_StatusRecords.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO Rent_Status 
+(Rent_ID, Rent_Date, Vid_ID, Mem_ID)
+VALUES('R001', '18-Oct-1993', 'V001', 'M001');
+
+INSERT INTO Rent_Status 
+(Rent_ID, Rent_Date,Vid_ID, Mem_ID)
+VALUES('R002', '18-Oct-1993', 'V003', 'M001');
+
+INSERT INTO Rent_Status 
+(Rent_ID, Rent_Date,Vid_ID, Mem_ID)
+VALUES('R003', '18-Oct-1993', 'V004', 'M001');
+
+INSERT INTO Rent_Status 
+(Rent_ID, Rent_Date, Return_Date,Vid_ID, Mem_ID)
+VALUES('R004', '21-Oct-1993', '28-Oct-1993', 'V006', 'M002');
+
+INSERT INTO Rent_Status 
+(Rent_ID, Rent_Date,Vid_ID, Mem_ID)
+VALUES('R005', '18-Oct-1993', 'V002', 'M001');
+
+INSERT INTO Rent_Status 
+(Rent_ID, Rent_Date,Vid_ID, Mem_ID)
+VALUES('R006', '18-Oct-1993', 'V005', 'M001');
