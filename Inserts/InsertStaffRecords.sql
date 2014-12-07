0e3da3bc1dfded08662620125b3b22dc84a103f8
jenapung <ahh.its.nena@hotmail.com>
2014-12-07 17:40:44 -0600

    Update InsertStaffRecords.sql

--- Inserts/InsertStaffRecords.sql
+++ Inserts/InsertStaffRecords.sql
@@ -1,20 +1,29 @@
-INSERT INTO Staff 
-VALUES ('S001','Becky',' Johnson', 'Manager', 'St.Cloud', '320', '5555555', 15)
 
 INSERT INTO Staff 
+(Staff_ID, Staff_Fn, Staff_Ln, Position, Staff_City, Staff_area, Staff_Phone, Salary)
+VALUES ('S001','Becky',' Johnson', 'Manager', 'St.Cloud', '320', '5555555', 15);
+
+INSERT INTO Staff 
+(Staff_ID, Staff_Fn, Staff_Ln, Position, Staff_City, Staff_area, Staff_Phone, Salary)
 VALUES ('S002', 'Sarah', 'Anderson', 'Manager', 'Sauk Rapids', '320', '5555557', 15);
 
 INSERT INTO Staff 
-VALUES('S003', 'Randy', 'Goodman', 'Manager', 'Sartell', '320', '5555558', 15);
+(Staff_ID, Staff_Fn, Staff_Ln, Position, Staff_City, Staff_area, Staff_Phone, Salary)
+VALUES ('S003', 'Randy', 'Goodman', 'Manager', 'Sartell', '320', '5555558', 15);
 
 INSERT INTO Staff 
-VALUES('S004', 'Susan', 'Rodriguez', 'Manager', 'Waite Park', '320', '5555559', 15);
+(Staff_ID, Staff_Fn, Staff_Ln, Position, Staff_City, Staff_area, Staff_Phone, Salary)
+VALUES ('S004', 'Susan', 'Rodriguez', 'Manager', 'Waite Park', '320', '5555559', 15);
 
 INSERT INTO Staff
-VALUES('S005', 'Robert', 'Mann', 'Manager', 'Little Falls', '320', '5555566', 15);
+(Staff_ID, Staff_Fn, Staff_Ln, Position, Staff_City, Staff_area, Staff_Phone, Salary)
+VALUES ('S005', 'Robert', 'Mann', 'Manager', 'Little Falls', '320', '5555566', 15);
 
 INSERT INTO Staff
-VALUES('S006', 'Daniel', 'Monroe', 'Cashier', 'St Cloud', '763', '5555567', 10);
+(Staff_ID, Staff_Fn, Staff_Ln, Position, Staff_City, Staff_area, Staff_Phone, Salary)
+VALUES ('S006', 'Daniel', 'Monroe', 'Cashier', 'St Cloud', '763', '5555567', 10);
 
 INSERT INTO Staff 
-VALUES('S007', 'Laura', 'Baker', 'Cashier', 'Sartell', '651', '5555568', 10);
+(Staff_ID, Staff_Fn, Staff_Ln, Position, Staff_City, Staff_area, Staff_Phone, Salary)
+VALUES ('S007', 'Laura', 'Baker', 'Cashier', 'Sartell', '651', '5555568', 10);
+
