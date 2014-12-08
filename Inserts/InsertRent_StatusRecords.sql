4a12342c171f4d1c2671fe7fc08294b6eb2a9452
Karl Konz <HelloGitWorld@gmail.com>
2014-12-07 20:43:19 -0600

    Update InsertRent_StatusRecords.sql

--- Inserts/InsertRent_StatusRecords.sql
+++ Inserts/InsertRent_StatusRecords.sql
@@ -1,24 +1,24 @@
 
 INSERT INTO Rent_Status 
 (Rent_ID, Rent_Date, Vid_ID, Mem_ID)
-VALUES('R001', '18-Oct-1993', 'V001', 'M001');
+VALUES('R001', '18-Oct-2014', 'V001', 'M001');
 
 INSERT INTO Rent_Status 
 (Rent_ID, Rent_Date,Vid_ID, Mem_ID)
-VALUES('R002', '18-Oct-1993', 'V003', 'M001');
+VALUES('R002', '18-Oct-2014', 'V003', 'M001');
 
 INSERT INTO Rent_Status 
 (Rent_ID, Rent_Date,Vid_ID, Mem_ID)
-VALUES('R003', '18-Oct-1993', 'V004', 'M001');
+VALUES('R003', '18-Oct-2014', 'V004', 'M001');
 
 INSERT INTO Rent_Status 
 (Rent_ID, Rent_Date, Return_Date,Vid_ID, Mem_ID)
-VALUES('R004', '21-Oct-1993', '28-Oct-1993', 'V006', 'M002');
+VALUES('R004', '21-Oct-2014', '28-Oct-1993', 'V006', 'M002');
 
 INSERT INTO Rent_Status 
 (Rent_ID, Rent_Date,Vid_ID, Mem_ID)
-VALUES('R005', '18-Oct-1993', 'V002', 'M001');
+VALUES('R005', '18-Oct-2014', 'V002', 'M001');
 
 INSERT INTO Rent_Status 
 (Rent_ID, Rent_Date,Vid_ID, Mem_ID)
-VALUES('R006', '18-Oct-1993', 'V005', 'M001');
+VALUES('R006', '18-Oct-2014', 'V005', 'M001');
