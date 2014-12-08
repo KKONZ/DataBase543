86fb2b43f647457750d48b4dd2c7b103d3dbd7ca
Karl Konz <HelloGitWorld@gmail.com>
2014-12-08 00:23:10 -0600

    Create AckChkoutTrg.sql

---
+++ PLSQL/AckChkoutTrg.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE TRIGGER  Chkout_Message_Trg
+after insert on Rent_Status 
+For each row
+
+BEGIN
+ --- prints from the server
+DBMS_OUTPUT.PUT_LINE('Check out is complete, thank you!');
+
+End;
+/ 
+show error;
