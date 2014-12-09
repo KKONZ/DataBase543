828b1cdc7110512de7094a7dfac9b88309967711
Karl Konz <HelloGitWorld@gmail.com>
2014-12-09 02:24:06 -0600

    Create Isolate1Day.sql

---
+++ Tests/Isolate1Day.sql
@@ -0,0 +1,17 @@
+select * from user_objects;
+
+--- make sure that 1Day trigger is the only one 
+-- delete trigger;
+Insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Vid_Id, Mem_ID)
+VALUES
+('R100', '04-Dec-2014', 'RC001', 'V002', 'M004);
+
+select * from Rent_Status;
+Select * from Member;
+
+Insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Vid_Id, Mem_ID)
+VALUES
+('R100', '04-Dec-2014', 'RC001', 'V002', 'M004);
+
+select * from Rent_Status;
+Select * from Member;
