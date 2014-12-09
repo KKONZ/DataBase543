b855bab7dce10705b15d4b55d5c39206e42390e1
Karl Konz <HelloGitWorld@gmail.com>
2014-12-09 11:28:50 -0600

    Update last_accrual_1day_trg.sql

--- PLSQL/last_accrual_1day_trg.sql
+++ PLSQL/last_accrual_1day_trg.sql
@@ -1,7 +1,7 @@
 create or replace trigger last_accrual_1Day_trg
 before insert or update on rent_status for each row
-declare rent_length binary_Integer;
- accrual_length binary_Integer;
+declare rent_length binary_Integer,
+         accrual_length binary_Integer;
 begin
 SELECT sum(TRUNC(sysdate) - TRUNC(rent_date)) into rent_length
 FROM rent_status
@@ -9,7 +9,7 @@ where return_date IS Null
 and mem_id =:new.mem_id
 and rent_code = 'RC001';
   if last_accrual IS NOT NULL then
-    select sum(TRUNC(sysdate)-TRUNC(Last_accrual)) into accrual_length from rent_status
+    select sum(TRUNC(sysdate)-TRUNC(Last_accrual)) into accrual_length from rent_status;
    
     update 
     member
