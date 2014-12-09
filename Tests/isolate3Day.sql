dd034a5e3d39fd1a22ab186e4a9e2f526338f057
Karl Konz <HelloGitWorld@gmail.com>
2014-12-09 02:37:22 -0600

    Create isolate3Day.sql

---
+++ Tests/isolate3Day.sql
@@ -0,0 +1,15 @@
+drop trigger last_accrual_1day_trg
+
+Insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Vid_Id, Mem_ID)
+VALUES
+('R300', '04-Dec-2014', 'RC001', 'V002', 'M004');
+
+select * from Rent_Status;
+Select * from Member;
+
+Insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Vid_Id, Mem_ID)
+VALUES
+('R301', sysdate, 'RC001', 'V004', 'M004');
+
+select * from Rent_Status;
+Select * from Member;
