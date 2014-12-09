b55a57465c4960e0716138e93bffc33fb9c653ea
Karl Konz <HelloGitWorld@gmail.com>
2014-12-09 03:00:04 -0600

    Update Isolate3DayT1.sql

--- Tests/Isolate3DayT1.sql
+++ Tests/Isolate3DayT1.sql
@@ -4,14 +4,14 @@ drop trigger late_fee_new_rls_trg
 
 Insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Vid_Id, Mem_ID)
 VALUES
-('R300', '04-Dec-2014', 'RC001', 'V002', 'M004');
+('R300', '04-Dec-2014', 'RC003', 'V002', 'M004');
 
 select * from Rent_Status;
 Select * from Member;
 
 Insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Vid_Id, Mem_ID)
 VALUES
-('R301', sysdate, 'RC001', 'V004', 'M004');
+('R301', sysdate, 'RC003', 'V004', 'M004');
 
 select * from Rent_Status;
 Select * from Member;
