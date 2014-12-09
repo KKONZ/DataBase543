d15584481414669c408c5a5d3498a7237ca28449
Karl Konz <HelloGitWorld@gmail.com>
2014-12-09 03:07:15 -0600

    Create Isolate1DayT2.sql

---
+++ Tests/Isolate1DayT2.sql
@@ -0,0 +1,21 @@
+
+
+
+insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Mem_ID) VALUES ('R007',sysdate, 'RC005', 'M001');
+
+
+select * from member;
+select * from rent_status;
+
+delete on Rent_status where Rent_ID = 'R007';
+
+
+---load triggers for rc 1, 3, 5
+
+insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Mem_ID) VALUES ('R007',sysdate, 'RC005', 'M001');
+
+
+select * from member;
+select * from rent_status;
+
+delete on Rent_status where Rent_ID = 'R007';
