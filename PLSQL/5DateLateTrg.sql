0ef7084adb847f13a32b85efb3b14c550d4e89bc
Karl Konz <HelloGitWorld@gmail.com>
2014-12-08 23:46:30 -0600

    Create 5DateLateTrg.sql

---
+++ PLSQL/5DateLateTrg.sql
@@ -0,0 +1,20 @@
+create or replace trigger late_fee_new_5day_trg
+before insert on rent_status for each row
+declare rent_length binary_Integer;
+begin
+SELECT sum(TRUNC(sysdate) - TRUNC(rent_date)) into rent_length
+FROM rent_status
+where return_date IS Null
+and mem_id =:new.mem_id
+and rent_code = 'RC005'; 
+if (rent_length) > 5
+then
+update
+member
+set balance = balance +  (rent_length * 2)
+where mem_id=:new.mem_id;
+else
+dbms_output.put_line('no new charges');
+end if;
+end;
+/
