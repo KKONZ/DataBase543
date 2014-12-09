7daa6c2a3e76864c99a23531edb09765d205b0ab
Karl Konz <HelloGitWorld@gmail.com>
2014-12-09 01:46:23 -0600

    Update TestMaxBalance.sql

--- Tests/TestMaxBalance.sql
+++ Tests/TestMaxBalance.sql
@@ -1,3 +1,6 @@
+update Member 
+set Balance = 50
+where Mem_ID = 'M002';
 
 ---this example should throw an error
 insert into Rent_Status (Rent_ID, Rent_Date, Mem_ID) VALUES ('R007',sysdate, 'M002');
