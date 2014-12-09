c24d25be195993ba8c651bba66346d92528b56e9
Karl Konz <HelloGitWorld@gmail.com>
2014-12-08 23:45:52 -0600

    Create 3DaysLate.sql

---
+++ PLSQL/3DaysLate.sql
@@ -0,0 +1,21 @@
+create or replace trigger late_fee_new_3day_trg
+before insert on rent_status for each row
+declare rent_length binary_Integer;
+begin
+SELECT sum(TRUNC(sysdate) - TRUNC(rent_date)) into rent_length
+FROM rent_status
+where return_date IS Null
+and mem_id =:new.mem_id
+and rent_code = 'RC003';
+ 
+if (rent_length) > 3
+then
+update
+member
+set balance = balance +  (rent_length * 2)
+where mem_id=:new.mem_id;
+else
+dbms_output.put_line('no new charges');
+end if;
+end;
+/
