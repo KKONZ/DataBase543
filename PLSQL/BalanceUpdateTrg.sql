acef14e609da5093679c21fbf162188f6a0a35db
Karl Konz <HelloGitWorld@gmail.com>
2014-12-08 23:44:59 -0600

    Update BalanceUpdateTrg.sql

--- PLSQL/BalanceUpdateTrg.sql
+++ PLSQL/BalanceUpdateTrg.sql
@@ -1,40 +1,24 @@
-
-create or replace trigger late_fee_trg
-
+create or replace trigger late_fee_new_rls_trg
 before insert on rent_status for each row
-
 declare rent_length binary_Integer;
-
 begin
-
-SELECT sum(TRUNC(sysdate) - TRUNC(rent_date)) into rent_length 
-
-FROM rent_status, member
-
-where member.mem_id = rent_status.mem_id
-
-and return_date IS Null 
-
-and rent_status.mem_id =:new.mem_id;
-
-
-if (rent_length) > 2 then
-
+SELECT sum(TRUNC(sysdate) - TRUNC(rent_date)) into rent_length
+FROM rent_status
+where return_date IS Null
+and mem_id =:new.mem_id
+and rent_code = 'RC001';
+ 
+if (rent_length) > 1 
+then
 update
-
-member 
-
-set balance = (rent_length * 2);
-
-else 
-
+member
+set balance = balance +  (rent_length * 2)
+where mem_id=:new.mem_id;
+else
 dbms_output.put_line('no new charges');
-
 end if;
-
 end;
-
 /
 
-show errors
 
+--- DELETE THIS TRIGGER AFTER DEMO IF NEW TRIGGER WORKS----
