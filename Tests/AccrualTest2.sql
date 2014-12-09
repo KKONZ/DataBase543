e4c3d37f942b8a783c81e28925ec82126dbede9b
Karl Konz <HelloGitWorld@gmail.com>
2014-12-09 07:48:16 -0600

    Update AccrualTest2.sql

--- Tests/AccrualTest2.sql
+++ Tests/AccrualTest2.sql
@@ -1,57 +1,8 @@
-DROP TABLE Rent_Status
-CASCADE CONSTRAINT;
+insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Mem_ID) VALUES ('R007',sysdate, 'RC005', 'M001');
+delete on Rent_status where Rent_ID = 'R007';
 
-CREATE TABLE Rent_Status(
-Rent_ID VARCHAR2(10) CONSTRAINT Rent_ID_PK PRIMARY KEY,
-Rent_Date DATE,
-Return_Date DATE,
+insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Mem_ID) VALUES ('R007',sysdate, 'RC005', 'M001');
+delete on Rent_status where Rent_ID = 'R007';
 
-
---- new column to keep track of accrual dates
-last_accrual DATE;
-
-
-Rent_code VARCHAR2(10),
-CONSTRAINT Rent_Code_FK1 FOREIGN KEY(Rent_Code) REFERENCES Rental_Code(Rent_Code),
-Vid_ID VARCHAR2(10),
-CONSTRAINT Vid_ID_FK2 FOREIGN KEY (Vid_ID) REFERENCES Video(Vid_ID),
-Mem_ID VARCHAR2(10),
-CONSTRAINT Mem_ID_FK3 FOREIGN KEY (Mem_ID) REFERENCES
-Member(Mem_ID));
-
-
---compare the outcome of 1st three rent codes triggers 
---to the accrual triggers
-
-
-INSERT INTO Rent_Status 
-(Rent_ID, Rent_Date, Vid_ID, Mem_ID)
-VALUES
-('R007', '07-Dec-2014', 'V001', 'M002');
-INSERT INTO Rent_Status 
-(Rent_ID, Rent_Date,Vid_ID, Mem_ID)
-VALUES
-('R008', '08-Dec-2014’', 'V005', 'M002');
-INSERT INTO Rent_Status 
-(Rent_ID, Rent_Date,Vid_ID, Mem_ID)
-VALUES
-('R009', '09-Dec-2014', 'V006', 'M002');
-INSERT INTO Rent_Status 
-(Rent_ID, Rent_Date,Vid_ID, Mem_ID)
-VALUES
-('R010', '08-Dec-2014', 'V007', 'M002');
-
-INSERT INTO Rent_Status 
-(Rent_ID, Rent_Date,Vid_ID, Mem_ID)
-VALUES
-('R011', '08-Dec-2014', 'V007', 'M002');
-
-
-
---- after test 
-delete Rent_Status where Rent_ID = 'R007';
-delete Rent_Status where Rent_ID = 'R008';
-delete Rent_Status where Rent_ID = 'R009';
-delete Rent_Status where Rent_ID = 'R010';
-
-
+select * from Member;
+select * from Rent_Status;
