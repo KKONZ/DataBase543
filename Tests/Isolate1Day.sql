c7bd68ab9c19160fd412dcf54aab1d9af307ee84
Karl Konz <HelloGitWorld@gmail.com>
2014-12-09 02:26:45 -0600

    Update Isolate1Day.sql

--- Tests/Isolate1Day.sql
+++ Tests/Isolate1Day.sql
@@ -1,17 +1,18 @@
-select * from user_objects;
+--- select * from user_objects;
 
---- make sure that 1Day trigger is the only one 
--- delete trigger;
+--- make sure that 1Day trigger is the only rent code trigger
+--- delete trigger;
+
 Insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Vid_Id, Mem_ID)
 VALUES
-('R100', '04-Dec-2014', 'RC001', 'V002', 'M004);
+('R100', '04-Dec-2014', 'RC001', 'V002', 'M004');
 
 select * from Rent_Status;
 Select * from Member;
 
 Insert into Rent_Status (Rent_ID, Rent_Date, Rent_Code, Vid_Id, Mem_ID)
 VALUES
-('R100', '04-Dec-2014', 'RC001', 'V002', 'M004);
+('R100', '04-Dec-2014', 'RC001', 'V002', 'M004');
 
 select * from Rent_Status;
 Select * from Member;
