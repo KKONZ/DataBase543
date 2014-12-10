1ce6b7054e975ecc88405f56c114c3ee0c2bbb63
Karl Konz <HelloGitWorld@gmail.com>
2014-12-10 17:44:39 -0600

    Update Rent_status_package.sql

--- Rent_status_package.sql
+++ Rent_status_package.sql
@@ -1,14 +1,9 @@
-create or replace package rent_status_package as
-type datetabtype is table of date index by binary_integer;
-type chartabtype is table of char(1) index by binary_integer;
-type rowidtabtype is table of rowid index by binary_integer;
---- balancetabtype is table of number index by binary_integer;
-rent_date_tab datetabtype;
-return_date_tab datetabtype;
-last_accrual_tab datetabtype;
-rent_id_tab rowidtabtype;
-changed_balance chartabtype;
-start_date_tab_size binary_integer;
-end;
-/
-show errors
+SQL> create or replace package Rent_status_packag
+  2  as 
+  3          type rid_Array is table of rowid index by binary_integer; 
+  4 
+  4          newRows rid_Array; 
+  5          empty   rid_Array; 
+  6  end; 
+  7  / 
+Package created.
