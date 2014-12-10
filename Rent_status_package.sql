92836b34bfb993fd28af091d622e09a59ba91137
Karl Konz <HelloGitWorld@gmail.com>
2014-12-10 17:49:55 -0600

    Delete Rent_status_package.sql

--- Rent_status_package.sql
+++
@@ -1,9 +0,0 @@
-SQL> create or replace package Rent_status_packag
-  2  as 
-  3          type rid_Array is table of rowid index by binary_integer; 
-  4 
-  4          newRows rid_Array; 
-  5          empty   rid_Array; 
-  6  end; 
-  7  / 
-Package created.
