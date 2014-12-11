be4d70608cb97943e00838fc0d008adec9e8ba38
Karl Konz <HelloGitWorld@gmail.com>
2014-12-10 19:06:34 -0600

    Update and rename late_fee_new_1day_trg.sql to day1_trg.sql

--- PLSQL/day1_trg.sql
+++ PLSQL/day1_trg.sql
@@ -1,4 +1,4 @@
-create or replace trigger late_fee_new_3day_trg
+create or replace trigger late_fee_new_1day_trg
 before insert or update on rent_status for each row
 declare rent_length binary_Integer;
 begin
@@ -6,16 +6,14 @@ SELECT sum(TRUNC(sysdate) - TRUNC(rent_date)) into rent_length
 FROM rent_status
 where return_date IS Null
 and mem_id =:new.mem_id
-and rent_code = 'RC003';
+and rent_code = 'RC001';
  
-if (rent_length) > 3
+if (rent_length) > 1
 then
 update
 member
 set balance = balance +  (rent_length * 2)
 where mem_id=:new.mem_id;
-else
-dbms_output.put_line('no new charges');
 end if;
 end;
 /
