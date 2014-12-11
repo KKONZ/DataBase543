1f433e8cdbf3e54fd77a6d38b43cf37f6a269a0f
Karl Konz <HelloGitWorld@gmail.com>
2014-12-10 20:13:40 -0600

    Update day3_trg.sql

--- PLSQL/day3_trg.sql
+++ PLSQL/day3_trg.sql
@@ -7,18 +7,16 @@ FROM rent_status
 where return_date IS Null
 and mem_id =:new.mem_id
 and last_accrual IS Null
+and rent_date > (sysdate-3)
 and rent_code = 'RC003';
- 
-if (rent_length) > 3
-then
+
 update
 member
-set balance = balance +  ((rent_length -1 )* 2)
+set balance = balance +  ((rent_length)* 2)
 where mem_id=:new.mem_id;
 update
 Rent_Status
 set last_accrual = sysdate
 where mem_id=:new.mem_id;
-end if;
 end;
 /
