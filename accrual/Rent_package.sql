b1017922f4d0041d961ec5557f5fca599594675f
Karl Konz <hellogitworld@gmail.com>
2014-12-11 18:16:36 -0600

    add LateFeeTrg to hist

--- accrual/Rent_package.sql
+++
@@ -1,9 +0,0 @@
-create or replace package Rent_pkg
- as 
-    type rid_Array is table of rowid index by binary_integer; 
-
-  newRows rid_Array; 
-   empty   rid_Array; 
-
-  end; 
-  / 
--- accrual/iPkg_trg.sql
+++
@@ -1,6 +0,0 @@
-create or replace trigger iPkg_trg
-before insert or update on parent 
-begin 
-  rent_pkg.newRows := rent_pkg.empty; 
-end; 
-/ 
