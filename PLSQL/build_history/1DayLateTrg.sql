676bfa5faab82e8f7df53987a6307b38c682766a
Karl Konz <hellogitworld@gmail.com>
2014-12-11 18:25:06 -0600

    rename and mv 1day trg

---
+++ PLSQL/build_history/1DayLateTrg.sql
@@ -0,0 +1,43 @@
+create or replace trigger late_fee_new_rls_trg
+before insert or update on rent_status for each row
+declare 
+PRAGMA AUTONOMOUS_TRANSACTION;
+rent_length binary_Integer;
+begin
+SELECT sum(TRUNC(sysdate) - TRUNC(rent_date)) into rent_length
+FROM rent_status
+where return_date IS Null
+and mem_id =:new.mem_id
+and rent_code = 'RC001'
+if (rent_length) > 1 
+then
+update
+member
+set balance = balance +  (rent_length * 2)
+where mem_id=:new.mem_id;
+update
+rent_status
+set last_accrual = sysdate
+where mem_id=:new.mem_id;
+
+else
+dbms_output.put_line('no new charges');
+end if;
+end;
+/
+show errors
+
+select * from member;
+
+/*
+run below if need to reset member balances to zero
+*/
+
+update member set balance =0 where mem_id ='M001';
+update member set balance =0 where mem_id ='M002';
+update member set balance =0 where mem_id ='M003';
+update member set balance =0 where mem_id ='M004';
+update member set balance =0 where mem_id ='M005';
+
+
+http://oracle-base.com/articles/9i/mutating-table-exceptions.php
