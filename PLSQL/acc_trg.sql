2a5d9b154fedd3a862d9fdde1029c9542b582605
Karl Konz <HelloGitWorld@gmail.com>
2014-12-11 18:45:28 -0600

    Update and rename accday1_trg.sql to acc_trg.sql

--- PLSQL/acc_trg.sql
+++ PLSQL/acc_trg.sql
@@ -6,15 +6,13 @@ SELECT sum(TRUNC(sysdate) - TRUNC(last_accrual)) into rent_length
 FROM rent_status
 where return_date IS Null
 and last_accrual IS NOT Null
-and mem_id =:new.mem_id
-
-and rent_code = 'RC001';
+and mem_id =:new.mem_id;
  
-if (rent_length) > 1
+if (rent_length) > 0
 then
 update
 member
-set balance = balance +  ((rent_length -1 )* 2)
+set balance = balance +  ((rent_length)* 2)
 where mem_id=:new.mem_id;
 update rent_status
 set last_accrual = sysdate;
