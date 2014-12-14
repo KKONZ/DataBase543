b07a7e5aa24c14b1839a79d3349dcb524475eab2
Karl Konz <HelloGitWorld@gmail.com>
2014-12-14 14:49:03 -0600

    Update SetUpDataBase.sql

--- Run/SetUpDataBase.sql
+++ Run/SetUpDataBase.sql
@@ -369,3 +369,85 @@ end;
 
 /
 show error;
+
+
+---accruals
+
+
+
+
+
+create or replace trigger acc1_trg
+before insert on rent_status for each row
+declare rent_length binary_Integer;
+begin
+SELECT sum(TRUNC(sysdate) - TRUNC(last_accrual_1)) into rent_length
+FROM rent_status
+where return_date IS Null 
+and last_accrual_1 IS NOT Null 
+and mem_id =:new.mem_id;
+ 
+if rent_length >0 then
+update
+member 
+set balance = balance + (rent_length * 2)
+where mem_id=:new.mem_id;
+
+update
+rent_status
+set last_accrual_1 = sysdate
+where mem_id=:new.mem_id;
+end if;
+end;
+/
+show errors
+
+create or replace trigger acc3_trg
+before insert on rent_status for each row
+declare rent_length binary_Integer;
+begin
+SELECT sum(TRUNC(sysdate) - TRUNC(last_accrual_3)) into rent_length
+FROM rent_status
+where return_date IS Null 
+and last_accrual_3 IS NOT Null 
+and mem_id =:new.mem_id;
+
+if rent_length >0 then
+update
+member 
+set balance = balance + (rent_length * 2)
+where mem_id=:new.mem_id;
+
+update
+rent_status
+set last_accrual_3 = sysdate
+where mem_id=:new.mem_id;
+end if;
+end;
+/
+show errors
+
+create or replace trigger acc5_trg
+before insert on rent_status for each row
+declare rent_length binary_Integer;
+begin
+SELECT sum(TRUNC(sysdate) - TRUNC(last_accrual_5)) into rent_length
+FROM rent_status
+where return_date IS Null 
+and last_accrual_5 IS NOT Null 
+and mem_id =:new.mem_id;
+
+if rent_length >0 then
+update
+member 
+set balance = balance + (rent_length * 2)
+where mem_id=:new.mem_id;
+
+update
+rent_status
+set last_accrual_5 = sysdate
+where mem_id=:new.mem_id;
+end if;
+end;
+/
+show errors
